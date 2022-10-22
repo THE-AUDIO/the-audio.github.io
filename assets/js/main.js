b8f4324bb82af1024dacb33c75d2fa8e5749766b
THE-AUDIO <nandraina.dev22@gmail.com>
2022-10-23 00:10:30 +0300

    portfolio 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 const navBarre = document.querySelector("nav")
 const search = document.querySelector(".searchBox")
 navBarre.addEventListener("mouseenter",()=>{
-    navBarre.style.background = "rgb(19, 53, 104)"
+    navBarre.style.background = "transparent";
 })
 
-search.addEventListener("click",()=>{
-    // search.style.transform = "rotateZ(360deg)"
-    console.log("teste");
-})
+// search.addEventListener("click",()=>{
+//     search.style.transform = "rotateZ(360deg)"
+// })
 
-navBarre.addEventListener("mouseout",()=>{
-    navBarre.style.background = "transparent"
-})
+// navBarre.addEventListener("mouseout",()=>{
+//     navBarre.style.background = "transparent"
+// })
